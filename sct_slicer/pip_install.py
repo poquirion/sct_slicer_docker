8ccf11d12e1e73e4151458ee0e5ee6e9012c4fae
Pierre-Olivier Quirion <pioliqui@gmail.com>
2017-10-27 16:29:28 -0400

    first commit

---
+++ sct_slicer/pip_install.py
@@ -0,0 +1,5 @@
+import pip,os 
+pip.main(['install', '-r','/usr/local/sct/install/requirements/requirementsConda.txt'])
+#os.chdir(os.getenv("SCT_DIR"))
+#pip.main(['install', '.'])
+#pip.main(['install', 'matplotlib'])
